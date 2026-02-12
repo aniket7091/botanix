47269bb453f0e0797c5d927b47b9c8abf72f8373
Aniket Kumar <aniketkumar@Anikets-MacBook-Air.local>
2026-02-12 18:00:47 +0530

    finshed tilled Bottom Navagation2

--- lib/screens/splash_screen/splash_screen.dart
+++ lib/screens/splash_screen/splash_screen.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:botanix/core/colors/App_colors.dart';
-import 'package:botanix/features/home/home.dart';
 import 'package:botanix/screens/onboarding_screen/onboarding_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
